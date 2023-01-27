58f53b5636f59d737e96b6d2d74b9c7d26ac56bf
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-01-27 11:05:32 +0530

    commit

--- src/componants/VideoDetail.jsx
+++ src/componants/VideoDetail.jsx
@@ -53,11 +53,11 @@ const VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
-        {console.log(videos)}
         {videos?
-          <Box sx={{width: "25%"}} px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
-          <Videos videos={videos?videos:null} direction="column" />
-        </Box> :null}
+          <Box sx={{ width:{xs:'60%',md:'25%'},marginLeft:{md:'1%',xs:'15%'}}} px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
+          <Videos  videos={videos?videos:null} direction="column" />
+          </Box> :null}
+        
       </Stack>
     </Box>
   );
